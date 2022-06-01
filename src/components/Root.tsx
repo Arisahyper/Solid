f949fafd01656a94ba5b53cd4d19db2394fbd998
Aqua Minato <sweet4182691@gmail.com>
2022-06-01 15:56:10 +0900

    centering

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -2,7 +2,7 @@ import { Link } from "solid-app-router";
 
 const Root = () => {
   return (
-    <div class="text-sky-600">
+    <div class="text-sky-600 text-center">
       <Link href="/">Root</Link>
       <Link href="/user">User</Link>
     </div>
