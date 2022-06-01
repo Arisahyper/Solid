399cbd574b3b8fc0102e4a8cdee43bd21360f8df
Aqua Minato <sweet4182691@gmail.com>
2022-06-01 15:21:15 +0900

    add Link

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,5 +1,12 @@
+import { Link } from "solid-app-router";
+
 const Root = () => {
-  return <div>Root</div>;
+  return (
+    <div class="text-sky-600">
+      <Link href="/">Root</Link>
+      <Link href="/user">User</Link>
+    </div>
+  )
 };
 
 export { Root };
